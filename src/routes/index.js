43f1215f4149e6fbbc8f34959c0aa5f578642ff2
aman kumar <aman163@gmail.com>
2025-06-21 02:32:58 +0530

    payment

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import authRouter from './auth.js';
 import productRouter from './product.js';
 import authenticationMiddleware from '../middleware/authentication.js';
+import paymentRouter from "./payment.js";
 
 const Router = express.Router();
 
 Router.use('/auth', authRouter);
 Router.use("/product", authenticationMiddleware, productRouter);
+Router.use("/payment", paymentRouter)
 
 export default Router;
